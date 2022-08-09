1e7a5f123232177ef1f8436af1e72d689ef5a44d
Jan <60812202+janrtvld@users.noreply.github.com>
2022-08-09 09:47:19 +0200

    fix: removed localhost tracking

--- client/src/utils/Analytics.ts
+++ client/src/utils/Analytics.ts
@@ -2,7 +2,6 @@ import Plausible from 'plausible-tracker'
 
 const { trackPageview, trackEvent } = Plausible({
   domain: 'demo.animo.id',
-  trackLocalhost: true,
 })
 
 export { trackPageview, trackEvent }
