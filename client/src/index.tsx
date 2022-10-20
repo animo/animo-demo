f338cd7ee7ecddb373f0313203c0e5bce0425ee2
Jan <jan@animo.id>
2022-10-20 16:34:37 +0200

    chore: removed strictmode
    Signed-off-by: Jan <jan@animo.id>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,4 @@
 import { KBarProvider } from 'kbar'
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
@@ -14,40 +13,38 @@ const { store, persistor } = Redux
 
 const container = document.getElementById('root')
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!) // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+const root = createRoot(container!)
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <KBarProvider
-            actions={[
-              {
-                id: 'invitation-type',
-                name: 'Change connection invitation type',
-                section: 'configuration',
-                keywords: 'invitation type',
-              },
-              {
-                id: 'theme',
-                name: 'Change theme…',
-                keywords: 'interface color dark light',
-                section: 'Preferences',
-              },
-              {
-                id: 'issue-credential-protocol-version',
-                name: 'Select credential protocol version',
-                keywords: 'issue credential protocol version',
-                section: 'configuration',
-              },
-            ]}
-            options={{ enableHistory: true, disableScrollbarManagement: true }}
-          >
-            <CommandBar />
-            <App />
-          </KBarProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <KBarProvider
+          actions={[
+            {
+              id: 'invitation-type',
+              name: 'Change connection invitation type',
+              section: 'configuration',
+              keywords: 'invitation type',
+            },
+            {
+              id: 'theme',
+              name: 'Change theme…',
+              keywords: 'interface color dark light',
+              section: 'Preferences',
+            },
+            {
+              id: 'issue-credential-protocol-version',
+              name: 'Select credential protocol version',
+              keywords: 'issue credential protocol version',
+              section: 'configuration',
+            },
+          ]}
+          options={{ enableHistory: true, disableScrollbarManagement: true }}
+        >
+          <CommandBar />
+          <App />
+        </KBarProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 )
