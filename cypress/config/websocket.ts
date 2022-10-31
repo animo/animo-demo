dd629ec63ca32d3bece821ec224e37ee47d40481
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-10-31 09:44:50 +0100

    Update cypress/config/websocket.ts
    Co-authored-by: Jan <jan@animo.id>

--- cypress/config/websocket.ts
+++ cypress/config/websocket.ts
@@ -3,7 +3,6 @@ const WS_URL = Cypress.env('wsUrl')
 import type { Event } from '../types/event'
 import type { WebSocketSubjectConfig } from 'rxjs/webSocket'
 
-// For full set of config values, check rxjs documentation
 export const webSocketConfig: WebSocketSubjectConfig<Event> = {
   url: WS_URL,
 }
