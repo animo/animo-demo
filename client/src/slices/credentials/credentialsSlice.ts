667b1928f28ec154a5f1bfd0beee46ad826b8f5f
Jim Ezesinachi <jim@animo.id>
2022-11-04 08:28:34 +0100

    refactor: renamed setCredential to addCredential
    Signed-off-by: Jim Ezesinachi <jim@animo.id>

--- client/src/slices/credentials/credentialsSlice.ts
+++ client/src/slices/credentials/credentialsSlice.ts
@@ -41,7 +41,7 @@ const credentialSlice = createSlice({
     setProtocolVersion: (state, action) => {
       state.protocolVersion = action.payload
     },
-    setCredential: (state, action) => {
+    addCredential: (state, action) => {
       state.credentials = [...state.credentials.filter((x) => x.id !== action.payload.id), action.payload]
     },
   },
@@ -107,6 +107,6 @@ const credentialSlice = createSlice({
   },
 })
 
-export const { clearCredentials, setProtocolVersion, setCredential } = credentialSlice.actions
+export const { clearCredentials, setProtocolVersion, addCredential } = credentialSlice.actions
 
 export default credentialSlice.reducer
