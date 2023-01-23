29a20438cab8d57d9e313fe1a19314390ff2c61c
Timo Glastra <timo@animo.id>
2023-01-23 12:32:39 +0100

    chore: add bc, holdr+ and orbit edge wallets (#152)
    Signed-off-by: Timo Glastra <timo@animo.id>
    
    Signed-off-by: Timo Glastra <timo@animo.id>

--- server/src/content/misc/Wallets.ts
+++ server/src/content/misc/Wallets.ts
@@ -1,5 +1,38 @@
 import type { Wallet } from '../types'
 
+const indicioHoldrPlus: Wallet = {
+  id: 5,
+  name: 'Holdr+',
+  organization: 'Indicio PBC.',
+  recommended: false,
+  icon: '/public/wallets/icon-holdr+.jpeg',
+  url: 'https://onelink.to/2wbkw7',
+  apple: 'https://apps.apple.com/us/app/holdr/id1620628623',
+  android: 'https://play.google.com/store/apps/details?id=tech.indicio.holdrplus',
+}
+
+const bcWallet: Wallet = {
+  id: 4,
+  name: 'BC Wallet',
+  organization: 'Province of British Columbia, Canada',
+  recommended: false,
+  icon: '/public/wallets/icon-bc-wallet.jpeg',
+  url: 'https://onelink.to/nk4wrv',
+  apple: 'https://apps.apple.com/us/app/bc-wallet/id1587380443',
+  android: 'https://play.google.com/store/apps/details?id=ca.bc.gov.BCWallet',
+}
+
+const orbitEdge: Wallet = {
+  id: 3,
+  name: 'Orbit Edge',
+  organization: 'Northern Block Inc.',
+  recommended: false,
+  icon: '/public/wallets/icon-orbit-edge.png',
+  url: 'https://onelink.to/bh2hcz',
+  apple: 'https://apps.apple.com/us/app/orbit-edge/id1508037063',
+  android: 'https://play.google.com/store/apps/details?id=com.northernblock',
+}
+
 export const Wallets: Wallet[] = [
   {
     id: 1,
